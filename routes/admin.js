203bc53498c59efacbd5ead35edcc6607ea1d00f
Sean Xiao <jean.paul.1685@outlook.com>
2021-04-28 17:31:28 -0400

    Refresh base code
    Refresh base code to new course code

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,15 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
 const router = express.Router();
-const path = require("path");
 
-const rootDir = require("../utils/path");
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
 
-router.use("/add-product", (req, res, next) => {
-    res.sendFile(path.join(rootDir, "/views", "add-product.html"));
-});
-
-router.post("/product", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
-});
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
 const router = express.Router();
-const path = require("path");
 
-const rootDir = require("../utils/path");
-
-router.get("/", (req, res, next) => {
-    res.sendFile(path.join(rootDir, "/views", "shop.html"));
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
